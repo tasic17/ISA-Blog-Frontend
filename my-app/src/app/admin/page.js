3252ba6859b0acc83b23311bc5acd6b622892760
Luka <luka0492@gmail.com>
2025-05-29 22:12:41 +0200

    .

--- my-app/src/app/admin/page.js
+++ my-app/src/app/admin/page.js
@@ -463,4 +463,4 @@ export default function AdminDashboard() {
                 </TabPane>
             </TabContent>
         </>
-    );
+    );}
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -152,3 +152,4 @@ export default function Home() {
         </section>
       </>
   );
+}
