6d6f2c78b0ea0c2eb48ef5d2353076404f1974ff
Luka <luka0492@gmail.com>
2025-05-31 11:59:35 +0200

    .

--- my-app/src/app/test2/page.js
+++
@@ -1,8 +0,0 @@
-export default function Test2Page() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Test Stranica</h1>
-      <p>Ovo je jednostavna test stranica bez client komponenti.</p>
-    </div>
-  );
-} 
